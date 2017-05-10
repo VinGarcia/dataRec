e48674704c2d0bca36a0ee622389624378c9ec89
Vinícius Garcia <vingarcia00@gmail.com>
2017-05-10 11:32:46 -0300

    Remove hardcoded arguments from main.py

--- main.py
+++ main.py
@@ -7,21 +7,26 @@ import json
 from search import Search
 
 if __name__ == '__main__':
-    # Validate parameters:
 
+    # Validate parameters:
     files = sys.argv[1:]
     if len(files) == 0:
         print("Usage: %s <file1.json> <file2.json> ..." % sys.argv[0])
         exit(1)
 
+    # Produce an empty search class:
     search = Search()
 
+    # Add the argument files to the class instance:
     for file in files:
         search.addDesc(file)
 
-    dev = os.open('/dev/sda5', os.O_RDONLY)
-
-    #os.lseek(dev, -1024*1024, os.SEEK_END)
+    # Ask for the partition address
+    sys.stdout.write("""
+    Please write the address
+    of the desired partition
+    to opened read-only: """)
+    dev = os.open(input(), os.O_RDONLY)
 
     print('starting search...')
 
